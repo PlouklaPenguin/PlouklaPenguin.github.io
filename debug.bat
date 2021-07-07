b4566056ae2124987a7f4b581fdbb94acb986d76
Sam <samjrobinson@protonmail.com>
2021-07-07 11:00:47 -0700

    first commit

---
+++ debug.bat
@@ -0,0 +1,1 @@
+cd Desktop/Desktop Folders/website
---
+++ testwhatfun.js
@@ -0,0 +1,13 @@
+
+function findEven(integers) {
+  
+}
+function findOutlier(integers){
+  //your code here
+  integers.find(findEven);
+
+
+
+}
+
+console.log(findOutlier([0, 1, 2]), 1);
